096a0958f05ead3b0b4c0cfa4f8c7269555b7357
Andrew J Lenards <andrew.lenards@gmail.com>
2013-05-30 16:29:44 -0700

    First crack `provenance` endpoint.

--- apps/axos_web/src/axos_web_provenance.erl
+++ apps/axos_web/src/axos_web_provenance.erl
@@ -1,9 +1,39 @@
 -module(axos_web_provenance).
--export([init/1, to_html/2]).
+-export([
+	init/1,
+	allowed_methods/2,
+	content_types_accepted/2,
+	content_types_provided/2
+	]).
+
+-export([
+	to_html/2
+	]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
-init(Target) -> {ok, Target}.
+-include("axos_web.hrl").
 
-to_html(ReqData, Target) ->
-    {"<html><body>Hello, provenance</body></html>", ReqData, Target}.
+-record(ctx, {}).
+
+init([]) ->
+	Ctx = #ctx{},
+	{ok, Ctx}.
+
+to_html(RD, Ctx) ->
+    {"<html><body>Hello, provenance</body></html>", RD, Ctx}.
+
+%% define the allowed HTTP methods
+%%
+%% Rd - request data, an opaque record, #wm_reqdata
+%% Ctx - resource's context that is provided
+allowed_methods(RD, Ctx) ->
+	{['POST'], RD, Ctx}.
+
+content_types_accepted(ReqData, Context) ->
+	{[{?MT_FORMENC, accept_form},
+	  {?MT_APPJSON, accept_json}], ReqData, Context}.
+
+content_types_provided(RD, Ctx) ->
+    {[ {?MT_APPJSON, to_json} ], RD, Ctx}.
+
