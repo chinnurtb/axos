a94e635055872cdbb55a9c58b1f1c34ce791f0f4
Andrew J Lenards <andrew.lenards@gmail.com>
2013-05-30 14:27:13 -0700

    Add start/0,stop/0,etc functions to main app module.

--- apps/axos/src/axos.erl
+++ apps/axos/src/axos.erl
@@ -7,6 +7,8 @@
         ]).
 
 %% Public API
+-export([start/0, start_link/0, stop/0]).
+
 
 %% @doc Pings a random vnode to make sure communication is functional
 ping() ->
@@ -14,3 +16,32 @@ ping() ->
     PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, axos),
     [{IndexNode, _Type}] = PrefList,
     riak_core_vnode_master:sync_spawn_command(IndexNode, ping, axos_vnode_master).
+
+%% pulled this from OJ Reeve's ``csd`` application
+
+ensure_started(App) ->
+	case application:start(App) of 
+		ok ->
+			ok;
+		{error, {already_started, App}} ->
+			ok
+	end.
+
+%% @spec start_link() -> {ok, Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+	ensure_started(crypto),
+	axos_sup:start_link().
+
+%% @spec start() -> ok
+%% @doc Start the csd_core server.
+start() ->
+	ensure_started(crypto),
+	application:start(axos).
+
+%% @spec stop() -> ok
+%% @doc Stop the csd_core server.
+stop() ->
+	Res = application:stop(axos),
+	application:stop(crypto),
+	Res.
